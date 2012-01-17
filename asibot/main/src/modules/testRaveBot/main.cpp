5e55b493b8c20fb9b08438e8a9f01b9275c85e41
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-01-17 12:30:32 +0000

    removed old test_ravebot

--- asibot/main/src/modules/testRaveBot/main.cpp
+++ asibot/main/src/modules/testRaveBot/main.cpp
@@ -1,18 +1,19 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /**
+ * 
+ * \defgroup testRaveBot
  *
- * \defgroup cartesianServer
- *
- * Creates an instance of CartesianBot.
+ * Creates an instance of RaveBot.
  *
  * <b> Legal </b>
  *
- * Copyright: 2012 (C) Universidad Carlos III de Madrid
+ * Copyright: 2011 (C) Universidad Carlos III de Madrid; 2006 (C) RobotCub Consortium
  *
  * Author: <a href="http://roboticslab.uc3m.es/roboticslab/persona_publ.php?id_pers=72">Juan G. Victores</a>
  *
- * Contrib: Paul Fitzpatrick (YARP sample code, email responses)
+ * Contrib: Paul Fitzpatrick (YARP sample code, email responses);
+ *          Rosen Diankov (OpenRAVE sample code, email responses)
  *
  * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see license/LGPL.TXT
  *
@@ -25,12 +26,12 @@
  *
  * yarp server &
  *
- * $ASIBOT_DIR/bin/cartesianServer
+ * $ASIBOT_DIR/bin/testRaveBot
  *
  * <b>Modify</b>
-
+ * 
  * This file can be edited at 
- * src/modules/cartesianServer/main.cpp
+ * src/modules/testRaveBot/main.cpp
  *
  */
 
