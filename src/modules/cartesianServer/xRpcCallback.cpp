453ce82745c977da6ff3bb80f697f34e169c37c4
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-01-24 16:35:08 +0000

    inverse kin position solver impemented and working

--- src/modules/cartesianServer/xRpcCallback.cpp
+++ src/modules/cartesianServer/xRpcCallback.cpp
@@ -37,7 +37,7 @@ bool xRpcCallback::read(ConnectionReader& connection) {
         Vector x,o;
         Bottle *lst = in.get(1).asList();
         printf("list of %d elements (5 needed: x y z oy' oz')\n", lst->size());
-        if(lst->size() < 5) {
+        if(lst->size() != 5) {
             out.addVocab(VOCAB_FAILED);
             out.write(*returnToSender);
             return false;
@@ -48,6 +48,30 @@ bool xRpcCallback::read(ConnectionReader& connection) {
         o.push_back(lst->get(3).asDouble());
         o.push_back(lst->get(4).asDouble());
         if(icart->goToPose(x,o)){
+            out.addVocab(VOCAB_OK);
+        } else
+            out.addVocab(VOCAB_FAILED);
+        out.write(*returnToSender);
+        return true;
+    } else if (choice==15) { ///////////////////////////////// 15 /////////////////////////////////
+        Vector xd,od,xdhat,odhat,qdhat;
+        Bottle *lst = in.get(1).asList();
+        printf("list of %d elements (5 needed: x y z oy' oz')\n", lst->size());
+        if(lst->size() != 5) {
+            out.addVocab(VOCAB_FAILED);
+            out.write(*returnToSender);
+            return false;
+        }
+        xd.push_back(lst->get(0).asDouble());
+        xd.push_back(lst->get(1).asDouble());
+        xd.push_back(lst->get(2).asDouble());
+        od.push_back(lst->get(3).asDouble());
+        od.push_back(lst->get(4).asDouble());
+        if(icart->askForPose(xd,od,xdhat,odhat,qdhat)){
+            Bottle& l1 = out.addList();
+            int i;
+            for (i = 0; i < qdhat.size(); i++)
+                l1.addDouble(qdhat[i]);
             out.addVocab(VOCAB_OK);
         } else
             out.addVocab(VOCAB_FAILED);
