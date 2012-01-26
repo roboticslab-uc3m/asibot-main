c61e52db66988b92f20f68872d8f35b567c1afed
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-01-26 15:09:42 +0000

    implemented law before J_inv

--- src/libraries/rlPlugins/cartesianbot/RateThreadImpl.cpp
+++ src/libraries/rlPlugins/cartesianbot/RateThreadImpl.cpp
@@ -23,35 +23,28 @@ void CartesianBot::run() {
             startTime = 0;
             pos->setPositionMode();
             cmc_status=0;
-        }/* else {
+        } else {
             //printf("Inside control loop moving.\n");
-            Twist T_desired = currentTrajectory->Vel(currentTime);
-            Frame F_desired = currentTrajectory->Pos(currentTime);
-            Twist T_current = diff(real_cartpos, F_desired);
-            for (unsigned int i=0; i<6; i++) {
-                T_current(i) *= GAIN;
-                T_current(i) += T_desired(i);
-            }
-            JntArray joint_vel(5);  // in radians
-//            ChainIkSolverVel_pinv iksolverv_pinv(theChain);
-//            iksolverv_pinv.CartToJnt(real_rad,T_current,joint_vel);
-            double cmc_qdot[5];
-            for (unsigned int i=0; i<5; i++)
-                cmc_qdot[i]=toDeg(joint_vel(i));
-            // Should normalize anyway (Find out the max, etc)
-            double max_qdot = 100;
-            for (int motor=0; motor<5; motor++)
-                if (fabs(cmc_qdot[motor])>max_qdot)
-                    max_qdot = fabs(cmc_qdot[motor]);
-            if (max_qdot > 100)
-                for (int motor=0; motor<5; motor++)
-                    cmc_qdot[motor]=(cmc_qdot[motor])*100.0/max_qdot;
-            if(!vel->velocityMove(cmc_qdot))
-                printf("[warning] CartesianBot::run() failed to velocityMove()\n");
-            currentTime = currentTime + TIMEINCREMENT;
-        }*/
+            yarp::sig::Vector xd,od,xd_dot,od_dot;
+            double currentTime = Time::now();
+            xd.push_back(trajX.get(startTime-currentTime));
+            xd.push_back(trajY.get(startTime-currentTime));
+            xd.push_back(trajZ.get(startTime-currentTime));
+            od.push_back(trajPitchP.get(startTime-currentTime));
+            od.push_back(trajRollP.get(startTime-currentTime));
+            yarp::sig::Vector ex = (xd - x);
+            yarp::sig::Vector eo = (od - o);
+            xd_dot.push_back(trajX.getd(startTime-Time::now()));
+            xd_dot.push_back(trajY.getd(startTime-Time::now()));
+            xd_dot.push_back(trajZ.getd(startTime-Time::now()));
+            od_dot.push_back(trajPitchP.getd(startTime-Time::now()));
+            od_dot.push_back(trajRollP.getd(startTime-Time::now()));
+            yarp::sig::Vector lawx = (ex * GAIN) + xd_dot;
+            yarp::sig::Vector lawo = (ex * GAIN) + od_dot;
+            
+        }
     } else {  // If it is stopped or breaked, reamain unchanged
-//j//        printf("Inside control loop stopped.\n");
+        // printf("Inside control loop stopped.\n");
     }
 }
 
