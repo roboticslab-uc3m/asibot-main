2ed9cad4dfa06520595a14de7de990fa5f6b4189
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-02-05 10:56:07 +0000

    cleaning

--- asibot/main/src/modules/cartesianServer/cartesianServer.h
+++ asibot/main/src/modules/cartesianServer/cartesianServer.h
@@ -20,25 +20,6 @@
 using namespace yarp::os;
 using namespace yarp::dev;
 
-/*class xCommPort : public BufferedPort<Bottle> {
-protected:
-    yarp::dev::ICartesianControl *icart;
-    virtual void onRead(Bottle& b) {
-        printf("[Debug] Data arrived on xCommPort\n");
-        printf("Recieved %s.\n", b.toString().c_str());
-        int choice = b.get(0).asInt();
-        if (b.get(0).getCode() != BOTTLE_TAG_INT) choice = -2;
-        if (choice==0) { ///////////////////////////////// x 0 /////////////////////////////////
-            bottle_x_o = theControlLoop.controlStat();
-        }
-    }
-public:
-    void setCartesianInterface(yarp::dev::ICartesianControl* _icart) {
-        icart = _icart;
-    }
-
-};*/
-
 class cartesianServer : public RFModule {
 protected:
     yarp::dev::PolyDriver cartesianDevice;
