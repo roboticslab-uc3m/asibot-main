a583869f322ec3156225d0591911b43fe707dec0
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-02-27 12:15:17 +0000

    file is created

--- src/modules/webInterface/WebResponder.cpp
+++ src/modules/webInterface/WebResponder.cpp
@@ -444,6 +444,7 @@ bool WebResponder::read(ConnectionReader& in) {
         return response.write(*out);
     } else if (code=="create.0") {
         ConstString nfile = request.find("nfile").asString();
+        appendToFile(nfile,"");
         printf("create.0 %s file.\n",nfile.c_str());
         response.addString(nfile);
         return response.write(*out);
