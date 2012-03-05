5da85fe43bbb1a229558cb6253e69082a58af0be
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-03-05 17:05:22 +0000

    AsibotTask grab from ASIBOT_ROOT/example/python

--- app/webInterface/user/AsibotTask.py
+++ app/webInterface/user/AsibotTask.py
@@ -5,5 +5,8 @@ if yarp.Network.checkNetwork() != True:
     print "[error] Please try running yarp server"
     quit()
 
+import sys, os
+asibot_root = os.environ['ASIBOT_ROOT']
+sys.path.append(os.path.join(asibot_root,'example/python'))
 from CartesianClient import CartesianClient  # imports the CartesianClient library
 
