39489e54e6cf7ddbac1d684bb88db1648bde1a70
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-03-12 09:22:38 +0000

    template format

--- app/webInterface/user/template.py
+++ app/webInterface/user/template.py
@@ -1,10 +1,11 @@
 from AsibotPy import *
-########################################
+#######################################
 home=[0,0,1.4,0,0]
-########################################
+#######################################
 simCart = CartesianClient()
-simCart.open('/ravebot')  # use '/canbot' for real
-########################################
+simCart.open('/ravebot')
+# use '/canbot' for real
+#######################################
 print 'hello, robot!'
 simCart.movl(home)  # defaults to 20 s
 Time.delay(20)
