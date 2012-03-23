9d6cb39b6ffbdecc9480394881fdcfe879be9114
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-03-23 10:14:00 +0000

    up forgotten file

---
+++ src/libraries/rlPlugins/ravebot/IControlLimitsImpl.cpp
@@ -0,0 +1,23 @@
+
+// -----------------------------------------------------------------------------
+
+#include "RaveBot.h"
+
+// ------------------- IControlLimits Related ------------------------------------
+
+bool RaveBot::setLimits(int axis, double min, double max) {
+    minLimit[axis] = min;
+    maxLimit[axis] = max;
+    printf("Range of axis %d set to: %f to %f\n",axis,min,max);
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+
+bool RaveBot::getLimits(int axis, double *min, double *max) {
+    *min = minLimit[axis];
+    *max = maxLimit[axis];
+    printf("Range of axis %d is: %f to %f.\n",axis,*min,*max);
+    return false;
+}
+
