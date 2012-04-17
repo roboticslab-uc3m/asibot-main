cf5dc6b761a86edfa7c79278e51f7adda629df0e
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-04-17 15:09:10 +0000

    less errors

--- src/libraries/CartesianServer/xRpcResponder.cpp
+++ src/libraries/CartesianServer/xRpcResponder.cpp
@@ -83,6 +83,7 @@ bool xRpcResponder::read(ConnectionReader& connection) {
             double qd[qdhat.size()];
             for (int i = 0; i < qdhat.size(); i++)
                 qd[i] = qdhat[i];
+            icart->stopControl(); // new!!!!
             ipos->setPositionMode();
             ipos->positionMove(qd);
             out.addVocab(VOCAB_OK);
