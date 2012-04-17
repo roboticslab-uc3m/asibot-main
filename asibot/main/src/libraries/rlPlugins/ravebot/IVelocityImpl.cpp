80fba0adcd1b77e1149fcd300eacfb5649517220
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-04-17 09:51:47 +0000

    returning to behavior where positionMove DOES NOT call setPositionMode; same for velocities

--- asibot/main/src/libraries/rlPlugins/ravebot/IVelocityImpl.cpp
+++ asibot/main/src/libraries/rlPlugins/ravebot/IVelocityImpl.cpp
@@ -6,7 +6,7 @@
 // ------------------ IVelocity Related ----------------------------------------
 
 bool RaveBot::setVelocityMode() {
-//    printf("RaveBot::setVelocityMode()\n");
+    printf("RaveBot::setVelocityMode()\n");
     if (modePosVel==1) return true;  // Simply return true if we were already in vel mode.
     // Do anything additional before setting flag to vel...
     modePosVel = 1;  // Set flag to vel.
@@ -17,11 +17,12 @@ bool RaveBot::setVelocityMode() {
 
 bool RaveBot::velocityMove(int j, double sp) {
     if(modePosVel!=1) {  // Check if we are in velocity mode.
-        if(!setVelocityMode()) return false;  // Don't do anything if we can't set velocity mode.
+        printf("[fail] RaveBot will not velocityMove as not in velocityMode\n");
+        return false;
     }
     if(sp>0) targetDeg[j]=maxLimit[j];
     else targetDeg[j]=minLimit[j];
-    // Let's not limit it for now:
+    // Let's not limit velocity it for now:
 //    if (sp>100) sp=100;
 //    else if (sp<-100) sp=-100;
     // Force it as is:
@@ -34,9 +35,10 @@ bool RaveBot::velocityMove(int j, double sp) {
 
 bool RaveBot::velocityMove(const double *sp) {
     if(modePosVel!=1) {  // Check if we are in velocity mode.
-        if(!setVelocityMode()) return false;  // Don't do anything if we can't set velocity mode.
+        printf("[fail] RaveBot will not velocityMove as not in velocityMode\n");
+        return false;
     }
-    // Let's not limit it for now:
+    // Let's not limit velocity it for now:
 //    double sp_limited[numMotors];
     printf("Vel:");
     for (unsigned int i=0; i<numMotors; i++) {
