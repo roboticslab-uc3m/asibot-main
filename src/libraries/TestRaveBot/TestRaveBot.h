6a26ca8764371aeddfba35b0fbc6183d440b17b8
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-04-17 14:06:51 +0000

    moving class to lib

---
+++ src/libraries/TestRaveBot/TestRaveBot.h
@@ -0,0 +1,34 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __TEST_RAVEBOT__
+#define __TEST_RAVEBOT__
+
+#include <yarp/os/RFModule.h>
+#include <yarp/os/Module.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/BufferedPort.h>
+
+#include <yarp/dev/PolyDriver.h>
+
+#define DEFAULT_ROBOT_NAME "ravebot"
+
+using namespace yarp::os;
+using namespace yarp::dev;
+
+class TestRaveBot : public RFModule {
+protected:
+    yarp::dev::PolyDriver robotDevice;
+
+//    double getPeriod();
+    bool updateModule();
+//    bool interruptModule();
+//    int period;
+
+public:
+    TestRaveBot();
+    bool configure(ResourceFinder &rf);
+};
+
+#endif
+
