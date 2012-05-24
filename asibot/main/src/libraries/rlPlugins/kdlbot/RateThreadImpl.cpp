005853e35839c0426cb0d4e94bea06038f8ce476
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-05-24 10:28:41 +0000

    kdlbot with movl, but does not respect duration or orientation, and possible small mem leak on the interpolator

--- asibot/main/src/libraries/rlPlugins/kdlbot/RateThreadImpl.cpp
+++ asibot/main/src/libraries/rlPlugins/kdlbot/RateThreadImpl.cpp
@@ -31,19 +31,58 @@ void KdlBot::run() {
         yarp::sig::Vector realUnits(numMotors,dRealUnits);
         yarp::sig::Vector x,o;
         fwdKin(realUnits,x,o);
-        bool done = true;
-        if(fabs(x[0]-targetX[0])>CARTPOS_PRECISION) done = false;
-        if(fabs(x[1]-targetX[1])>CARTPOS_PRECISION) done = false;
-        if(fabs(x[2]-targetX[2])>CARTPOS_PRECISION) done = false;
-        if((withOri)&&(fabs(o[0]-targetO[0])>CARTORI_PRECISION)) done = false;
-        if((withOri)&&(fabs(o[1]-targetO[1])>CARTORI_PRECISION)) done = false;
+        bool done = false;
+        KDL::Vector currentX(x[0],x[1],x[2]);
+        KDL::Rotation currentO;
+        if (angleRepr == "eulerYZ") {  // ASIBOT
+            KDL::Rotation currentO = Rotation::EulerZYZ(atan2(x[1],x[0]),toRad(o[0]), toRad(o[1]));
+        } else if (angleRepr == "eulerZYZ") {
+            KDL::Rotation currentO = Rotation::EulerZYZ(toRad(o[0]), toRad(o[1]), toRad(o[2]));
+        } else {  // axisAngle, etc.
+            printf("[warning] KDL no compatible angleRepr\n");
+        }
+        KDL::Frame currentF(currentO,currentX);
+
+        if(KDL::Equal(currentF, targetF, DEFAULT_EPS)) done = true;
         if (done) {
             printf("Target reached in %f.\n",Time::now()-startTime);
             startTime = 0;
             pos->setPositionMode();
             cmc_status=0;
+//            delete _orient;
+//            _orient=0;
         } else {
             //printf("Inside control loop moving.\n");
+ 
+/*            yarp::sig::Vector xP,xPd,xPdotd,eP,lawxP;
+            lawxP.resize(3);
+            lawxP = (eP * GAIN * (cmcMs/1000.0)) + xPdotd;  // GAIN=0 => lawxP = xPdotd;
+            t = Ja_pinv * lawxP;*/
+
+//            double _eps = 0.00001;
+//            int _maxiter = 150;
+//            ChainIkSolverVel_wdls iksolverv_wdls(theChain, _eps, _maxiter);
+//            int ret_ik_v = iksolverv_wdls.CartToJnt(q_current,T_current,qdot_current);
+
+            ChainIkSolverVel_pinv iksolverv(theChain);
+
+            JntArray currentRads = JntArray(numMotors);
+            for (int motor=0; motor<numMotors; motor++) {
+                if(isPrismatic[motor]) currentRads(motor)=dRealUnits[motor];
+                else currentRads(motor)=toRad(dRealUnits[motor]);
+            }
+            KDL::Twist currentT = diff(currentF, targetF);
+            JntArray kdlqdot = JntArray(numMotors);
+            int ret = iksolverv.CartToJnt(currentRads,currentT,kdlqdot);
+
+            double qdot[numMotors];
+            for (int motor=0; motor<numMotors; motor++) {
+                if(isPrismatic[motor]) qdot[motor]=kdlqdot(motor);
+                else qdot[motor]=toDeg(kdlqdot(motor));
+            }  
+            if(!vel->velocityMove(qdot))
+                printf("GIGANTIC velocity WARNING\n");
+
         }
     } else {  // If it is stopped or breaked, reamain unchanged
         // printf("Inside control loop stopped.\n");
