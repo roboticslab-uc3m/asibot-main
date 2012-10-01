b347920d52877ef34480f28e07a0d2a292006ee8
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-10-01 15:21:46 +0000

    now you can create and erase several sboxes

--- src/libraries/RlPlugins/ravebot/WorldRpcResponder.cpp
+++ src/libraries/RlPlugins/ravebot/WorldRpcResponder.cpp
@@ -22,25 +22,31 @@ bool WorldRpcResponder::read(ConnectionReader& connection) {
             if (in.get(2).asString() == "sbox") {
                 { // lock the environment!           
                 OpenRAVE::EnvironmentMutex::scoped_lock lock(pEnv->GetMutex());
-                kinBodyPtr = RaveCreateKinBody(pEnv,"");
+                KinBodyPtr sboxKinBodyPtr = RaveCreateKinBody(pEnv,"");
                 ConstString sboxName("sbox");
-                sboxName += ConstString::toString(boxCounter+1);
-                kinBodyPtr->SetName(sboxName.c_str());
+                sboxName += ConstString::toString(sboxKinBodyPtrs.size()+1);
+                sboxKinBodyPtr->SetName(sboxName.c_str());
                 std::vector<AABB> boxes(1);
                 boxes[0].extents = Vector(in.get(3).asDouble(), in.get(4).asDouble(), in.get(5).asDouble());
                 boxes[0].pos = Vector(in.get(6).asDouble(), in.get(7).asDouble(), in.get(8).asDouble());
-                kinBodyPtr->InitFromBoxes(boxes,true); 
-                pEnv->Add(kinBodyPtr,true);
-                boxCounter++;
+                sboxKinBodyPtr->InitFromBoxes(boxes,true); 
+                pEnv->Add(sboxKinBodyPtr,true);
+                sboxKinBodyPtrs.push_back(sboxKinBodyPtr);
                 }  // the environment is not locked anymore
                 out.addVocab(VOCAB_OK);
             } else out.addVocab(VOCAB_FAILED);
+        } else if ((in.get(1).asString()=="del")&&(in.get(2).asString()=="all")) {
+            for (unsigned int i=0;i<sboxKinBodyPtrs.size();i++) {
+                pEnv->Remove(sboxKinBodyPtrs[i]);
+            }
+            sboxKinBodyPtrs.clear();
+            out.addVocab(VOCAB_OK);
         } else if (in.get(1).asString()=="grab") {
             if (in.get(2).asInt()==1) {
-                pRobot->Grab(kinBodyPtr);
+                //pRobot->Grab(kinBodyPtr);
                 out.addVocab(VOCAB_OK);
             } else if (in.get(2).asInt()==0) {
-                pRobot->Release(kinBodyPtr);
+                //pRobot->Release(kinBodyPtr);
                 out.addVocab(VOCAB_OK);
             } else out.addVocab(VOCAB_FAILED);
         } else
@@ -51,14 +57,6 @@ bool WorldRpcResponder::read(ConnectionReader& connection) {
     out.addVocab(VOCAB_FAILED);
     out.write(*returnToSender);
     return false;
-}
-
-/************************************************************************/
-
-void WorldRpcResponder::resetCounters() {
-    boxCounter = 0; sboxCounter = 0;
-    cylCounter = 0; scylCounter = 0;
-    sphCounter = 0; ssphCounter = 0;
 }
 
 /************************************************************************/
