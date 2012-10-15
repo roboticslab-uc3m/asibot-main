4a27fc49a546fd98bdbeba85c03a71c42da2c873
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-10-15 14:55:57 +0000

    better

--- src/modules/colorSegmentor/FindRedCan.hpp
+++ src/modules/colorSegmentor/FindRedCan.hpp
@@ -1,14 +1,14 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#ifndef __FIND_RED_CAN_HPP__
-#define __FIND_RED_CAN_HPP__
+#ifndef __COLOR_SEGMENTOR_HPP__
+#define __COLOR_SEGMENTOR_HPP__
 
 #include "FinderThread.hpp"
 
 using namespace yarp::os;
 using namespace yarp::sig;
 
-class FindRedCan : public RFModule {
+class ColorSegmentor : public RFModule {
   private:
     FinderThread finderThread;
     //
