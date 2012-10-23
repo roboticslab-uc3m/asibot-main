139e697b9c6630d54ada4da612f94717bfd368f7
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-10-23 14:33:28 +0000

    udpate kdlbot to latest yarp

--- src/libraries/RlPlugins/kdlbot/KdlBot.h
+++ src/libraries/RlPlugins/kdlbot/KdlBot.h
@@ -603,6 +603,29 @@ class KdlBot : public DeviceDriver, public RateThread, public ICartesianControl 
     */
     virtual bool unregisterEvent(yarp::dev::CartesianEvent &event);
 
+    /**
+    * Tweak low-level controller's parameters. [wait for reply]
+    * @param options is a property-like bottle containing new values 
+    *                for the low-level controller's configuration.
+    * @return true/false on success/failure. 
+    *  
+    * @note This method is intended for accessing low-level 
+    *       controller's configuration.
+    */
+    virtual bool tweakSet(const yarp::os::Bottle &options);
+
+    /**
+    * Return low-level controller's parameters. [wait for reply]
+    * @param options is a property-like bottle containing the 
+    *                current values of the low-level controller's
+    *                configuration.
+    * @return true/false on success/failure. 
+    *  
+    * @note This method is intended for accessing low-level 
+    *       controller's configuration.
+    */
+    virtual bool tweakGet(yarp::os::Bottle &options);
+
 // -------- DeviceDriver declarations. Implementation in IDeviceImpl.cpp --------
 
     /**
