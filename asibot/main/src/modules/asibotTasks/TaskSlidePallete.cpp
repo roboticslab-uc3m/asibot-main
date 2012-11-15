803b3bb57b7b43696956d93ede6c98a04d22d99e
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-11-15 09:18:56 +0000

    gutsy move

---
+++ asibot/main/src/modules/asibotTasks/TaskSlidePallete.cpp
@@ -0,0 +1,54 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "TaskSlidePallete.h"
+
+/************************************************************************/
+TaskSlidePallete::TaskSlidePallete() { }
+
+/************************************************************************/
+bool TaskSlidePallete::open(const ConstString& serverPrefix) {
+    Property options;
+    options.put("device","remote_controlboard");
+    options.put("remote","/ravebot");
+    options.put("local","/local");
+    dd.open(options);
+    if(!dd.isValid()) {
+      printf("Robot device not available.\n");
+	  dd.close();
+      Network::fini();
+      return 1;
+    }
+    printf("[success] Opened connection with robot.\n");
+    bool ok = dd.view(pos);
+    if (!ok) {
+        printf("[warning] Problems acquiring robot interface\n");
+        return false;
+    } else printf("[success] Acquired robot interface\n");
+
+    return true;
+}
+
+/************************************************************************/
+bool TaskSlidePallete::close() {
+    dd.close();
+    printf("sent robot device close...\n");
+    return true;
+}
+
+/************************************************************************/
+bool TaskSlidePallete::run(const ConstString a, const ConstString b, const ConstString c, const ConstString d, const ConstString e, const ConstString f) {
+    printf("[success] TaskSlidePallete::run(): begin.\n");
+    pos->setPositionMode();
+    pos->positionMove(4,160);
+    bool done = false;
+    while(!done) {
+        pos->checkMotionDone(&done);
+        printf("Waiting 1 second...\n");
+        Time::delay(1);
+    }
+    printf("[success] TaskSlidePallete::run(): end.\n");
+    return true;
+}
+
+/************************************************************************/
+
