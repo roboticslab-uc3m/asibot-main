92a679234b1153ddc460177d2d2afc087605944e
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-12-03 11:43:00 +0000

    hacks for win32 which needs to know buffer size, set all to 100.

--- src/libraries/RlPlugins/kdlbot/ICartesianImpl.cpp
+++ src/libraries/RlPlugins/kdlbot/ICartesianImpl.cpp
@@ -24,7 +24,7 @@ bool KdlBot::getPose(const int axis, yarp::sig::Vector &x, yarp::sig::Vector &o,
 // ----------------------------------------------------------------------------- 
 
 bool KdlBot::getPose(yarp::sig::Vector &x, yarp::sig::Vector &o, yarp::os::Stamp *stamp) {
-    double dRealUnits[cmcNumMotors];
+    double dRealUnits[100];
     if(!enc->getEncoders(dRealUnits)) {
         printf("[warning] KdlBot::getPose() failed to getEncoders()\n");
         return false;
@@ -58,7 +58,7 @@ bool KdlBot::goToPoseSync(const yarp::sig::Vector &xd, const yarp::sig::Vector &
     targetF.p.data[1] = xd[1];
     targetF.p.data[2] = xd[2];
     if (angleRepr == "eulerYZ") {  // ASIBOT
-        targetF.M = Rotation::EulerZYZ(atan2(xd[1],xd[0]),toRad(od[0]), toRad(od[1]));
+        targetF.M = Rotation::EulerZYZ(::atan2(xd[1],xd[0]),toRad(od[0]), toRad(od[1]));
     } else if (angleRepr == "eulerZYZ") {
         targetF.M = Rotation::EulerZYZ(toRad(od[0]), toRad(od[1]), toRad(od[2]));
     } else {  // axisAngle, etc.
@@ -75,7 +75,7 @@ bool KdlBot::goToPoseSync(const yarp::sig::Vector &xd, const yarp::sig::Vector &
     initF.p.data[1] = x[1];
     initF.p.data[2] = x[2];
     if (angleRepr == "eulerYZ") {  // ASIBOT
-        initF.M = Rotation::EulerZYZ(atan2(x[1],x[0]),toRad(o[0]), toRad(o[1]));
+        initF.M = Rotation::EulerZYZ(::atan2(x[1],x[0]),toRad(o[0]), toRad(o[1]));
     } else if (angleRepr == "eulerZYZ") {
         initF.M = Rotation::EulerZYZ(toRad(o[0]), toRad(o[1]), toRad(o[2]));
     } else {  // axisAngle, etc.
@@ -121,7 +121,7 @@ bool KdlBot::getDesired(yarp::sig::Vector &xdhat, yarp::sig::Vector &odhat,
 bool KdlBot::askForPose(const yarp::sig::Vector &xd, const yarp::sig::Vector &od,
                             yarp::sig::Vector &xdhat, yarp::sig::Vector &odhat,
                             yarp::sig::Vector &qdhat) {
-    double currentUnits[cmcNumMotors];
+    double currentUnits[100];
     if(!enc->getEncoders(currentUnits)) {
         printf("[warning] KdlBot::getPose() failed to getEncoders()\n");
         return false;
@@ -137,7 +137,7 @@ bool KdlBot::askForPose(const yarp::sig::Vector &xd, const yarp::sig::Vector &od
     frameXd.p.data[1]=xd[1];
     frameXd.p.data[2]=xd[2];
     if (angleRepr == "eulerYZ") {
-        frameXd.M = Rotation::EulerZYZ(atan2(xd[1],xd[0]),toRad(od[0]), toRad(od[1]));
+        frameXd.M = Rotation::EulerZYZ(::atan2(xd[1],xd[0]),toRad(od[0]), toRad(od[1]));
     } else if (angleRepr == "eulerZYZ") {
         frameXd.M = Rotation::EulerZYZ(toRad(od[0]), toRad(od[1]), toRad(od[2]));
     } else {
