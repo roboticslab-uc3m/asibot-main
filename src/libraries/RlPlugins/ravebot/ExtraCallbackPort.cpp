92e641ed24b3a14e66995d500a53a58afa8153d3
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-12-13 16:45:27 +0000

    yay

--- src/libraries/RlPlugins/ravebot/ExtraCallbackPort.cpp
+++ src/libraries/RlPlugins/ravebot/ExtraCallbackPort.cpp
@@ -5,7 +5,9 @@
 /************************************************************************/
 
 void ExtraCallbackPort::onRead(Bottle& b) {
-    printf("[ExtraCallbackPort] Got %s\n", b.toString().c_str());
+    //printf("[ExtraCallbackPort] Got %s\n", b.toString().c_str());
+    for(int i=0;i<b.size();i++)
+        setEncExposed(i,b.get(i).asDouble());
 }
 
 /************************************************************************/
