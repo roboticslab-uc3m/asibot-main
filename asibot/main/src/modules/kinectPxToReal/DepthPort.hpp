d80b1e5cf89497b3c89708f6d18a2e81edc7c1cb
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2012-12-26 08:14:45 +0000

    go

--- asibot/main/src/modules/kinectPxToReal/DepthPort.hpp
+++ asibot/main/src/modules/kinectPxToReal/DepthPort.hpp
@@ -4,20 +4,20 @@
 #define __DEPTH_PORT_HPP__
 
 #include <yarp/os/all.h>
-#include <yarp/sig/Vector.h>
-#include <yarp/math/Math.h>
+//#include <yarp/sig/Vector.h>
+//#include <yarp/math/Math.h>
 
 #include "SharedArea.hpp"
 
-class DepthPort : public BufferedPort<ImageOf<PixelFloat>> {
+class DepthPort : public BufferedPort< ImageOf<PixelFloat> > {
     protected:
-        sharedArea *pMem;
+        SharedArea *pMem;
         virtual void onRead(ImageOf<PixelFloat>& depth) {
             printf("[DepthPort] Data arrived, height: %d, width: %d.\n",depth.height(),depth.width());
             pMem->setDepth(depth);
         }
     public:
-        void setSharedArea(sharedArea* _pMem) {
+        void setSharedArea(SharedArea* _pMem) {
             pMem = _pMem;
         }
 };
