7bc47176dee7f5db3bac5aae0235683be3650e0f
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-12-26 08:29:10 +0000

    cleaning

--- src/modules/kinectPxToReal/KinectPxToReal.cpp
+++ src/modules/kinectPxToReal/KinectPxToReal.cpp
@@ -41,40 +41,22 @@ bool KinectPxToReal::configure(ResourceFinder &rf) {
     callbackPort.open("/kinectPxToReal/state:i");
     outPort.open("/kinectPxToReal/state:o");
 
+    callbackPort.useCallback();
+
     return true;
 }
 
 /************************************************************************/
 
 bool KinectPxToReal::interruptModule() {
-    /*inDepth.interrupt();
-    inPort.interrupt();
+    depthPort.interrupt();
     callbackPort.interrupt();
-    inDepth.close();
-    inPort.close();
-    callbackPort.close();*/
+    outPort.interrupt();
+    depthPort.close();
+    callbackPort.close();
+    outPort.close();
     return true;
 }
-
-/************************************************************************/
-
-/*bool KinectPxToReal::getMatrixFromProperties(Searchable &options, ConstString &tag, yarp::sig::Matrix &H) {
-
-    Bottle *bH=options.find(tag).asList();
-    if (!bH) return false;
-    
-    int i=0;
-    int j=0;
-    H.zero();
-    for (int cnt=0; (cnt<bH->size()) && (cnt<H.rows()*H.cols()); cnt++) {    
-        H(i,j)=bH->get(cnt).asDouble();
-        if (++j>=H.cols()) {
-            i++;
-            j=0;
-        }
-    }
-    return true;
-}*/
 
 /************************************************************************/
 
