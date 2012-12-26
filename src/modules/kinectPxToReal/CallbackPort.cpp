ec0cbba2f7c68ad71668aff4df3d0dccf97b0b01
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-12-26 08:26:22 +0000

    cleaner

--- src/modules/kinectPxToReal/CallbackPort.cpp
+++ src/modules/kinectPxToReal/CallbackPort.cpp
@@ -13,32 +13,4 @@ void CallbackPort::setOutPort(Port* _outPort) {
 }
 
 /************************************************************************/
-//void CallbackPort::init(ResourceFinder &rf) {
-
-/*    while (pMem->getQ().size() == 0) {
-        Time::delay(0.5);
-        printf("Waiting for q...\n");
-    }
-
-    while (pMem->getF().size() == 0) {
-        Time::delay(0.5);
-        printf("Waiting for f...\n");
-    }
-
-    int period = rf.check("rate",20,"ms ratethread").asInt();
-    this->setRate(period);
-    this->start();*/
-//}
-
-/************************************************************************/
-//void CallbackPort::run() {
-    /*Vector predictFerr = pMachine.predict(pMem->getQ());
-    printf("predictFerr: %s\n",predictFerr.toString().c_str());
-    Vector currentF = pMem->getF();*/
-//    Vector& correctF = pfcPort->prepare();
-//    correctF = currentF - predictFerr;
-//    pfcPort->write();
-//}
-
-/************************************************************************/
 
