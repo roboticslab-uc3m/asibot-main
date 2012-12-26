13ee0f484516bd76486ba719c4152e5ee6b1fbb1
Juan G. Victores <jgvictores@users.noreply.github.com>
2012-12-26 08:14:45 +0000

    go

--- src/modules/kinectPxToReal/CallbackPort.cpp
+++ src/modules/kinectPxToReal/CallbackPort.cpp
@@ -1,21 +1,16 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#include "OutThread.hpp"
+#include "OutPort.hpp"
 
 /************************************************************************/
-void OutThread::setSharedArea(SharedArea* _pMem) {
+void OutPort::setSharedArea(SharedArea* _pMem) {
     pMem = _pMem;
 }
 
 /************************************************************************/
-void OutThread::setOutputPort(Port* _outPort) {
-    outPort = _outPort;
-}
+void OutPort::init(ResourceFinder &rf) {
 
-/************************************************************************/
-void OutThread::init(ResourceFinder &rf) {
-
-    while (pMem->getQ().size() == 0) {
+/*    while (pMem->getQ().size() == 0) {
         Time::delay(0.5);
         printf("Waiting for q...\n");
     }
@@ -27,11 +22,11 @@ void OutThread::init(ResourceFinder &rf) {
 
     int period = rf.check("rate",20,"ms ratethread").asInt();
     this->setRate(period);
-    this->start();
+    this->start();*/
 }
 
 /************************************************************************/
-void OutThread::run() {
+void OutPort::run() {
     /*Vector predictFerr = pMachine.predict(pMem->getQ());
     printf("predictFerr: %s\n",predictFerr.toString().c_str());
     Vector currentF = pMem->getF();*/
