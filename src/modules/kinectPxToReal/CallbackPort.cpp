603fa625aab8eb26be865d2b4ebff8263bb0b554
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-01-14 20:26:05 +0000

    pixel position casted to int to get depth values

--- src/modules/kinectPxToReal/CallbackPort.cpp
+++ src/modules/kinectPxToReal/CallbackPort.cpp
@@ -23,14 +23,14 @@ void CallbackPort::setOutPort(Port* _outPort) {
 /************************************************************************/
 
 void CallbackPort::onRead(Bottle& b) {
-    printf("[CallbackPort] Got %s\n", b.toString().c_str());
+    //fprintf(stdout,"[CallbackPort] Got %s\n", b.toString().c_str());
     Bottle outLists;
     for (int i=0; i<b.size(); i++) {
         Bottle* pxCoords = b.get(i).asList();
         double pxX = pxCoords->get(0).asDouble();
         double pxY = pxCoords->get(1).asDouble();
         ImageOf<PixelFloat> depth = pMem->getDepth();
-        double mmZ = depth(pxX,pxY);
+        double mmZ = depth((int)pxX,(int)pxY);  // maybe better do a mean around area?
         Bottle mmOut;
         double mmX = 1000.0 * (pxX - (cx * mmZ/1000.0)) / fx;
         double mmY = 1000.0 * (pxY - (cy * mmZ/1000.0)) / fy;
