eb5b81176e55fc82e556bebd89a2a8da8a1ae97e
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-04-06 16:14:57 +0000

    colorSegmentor works, colorDepth should too

--- src/modules/colorSegmentor/SegmentorThread.hpp
+++ src/modules/colorSegmentor/SegmentorThread.hpp
@@ -3,11 +3,11 @@
 #ifndef __SEGMENTOR_THREAD_HPP__
 #define __SEGMENTOR_THREAD_HPP__
 
-#include <yarp/os/RFModule.h>
-#include <yarp/os/Module.h>
+#include <yarp/os/BufferedPort.h>
 #include <yarp/os/Network.h>
 #include <yarp/os/Port.h>
-#include <yarp/os/BufferedPort.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/RateThread.h>
 
 #include <yarp/sig/all.h>
 
