20039642c84a3372c3fd25dd9da0fba28547b26c
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2013-04-09 13:14:41 +0000

    best api and doc and cpp example

--- asibot/main/src/libraries/ColorClientLib/ColorClient.cpp
+++ asibot/main/src/libraries/ColorClientLib/ColorClient.cpp
@@ -14,10 +14,10 @@ bool ColorClient::open(const char* serverPrefix, bool quiet) {
     ConstString serverStr(serverPrefix);
     serverStr += "/state:o";
     if (!Network::connect(serverStr,clientStr)) {
-        if (!isQuiet) fprintf(stderr,"[fail] Could not connect %s to %s.\n",serverStr.c_str(),clientStr.c_str());
+        if (!isQuiet) fprintf(stderr,"[ColorClient] fail: Could not connect %s to %s.\n",serverStr.c_str(),clientStr.c_str());
         return false;
     }
-    if (!isQuiet) printf("[ColorClient] %s reading from %s.\n",clientStr.c_str(),serverStr.c_str());
+    if (!isQuiet) printf("[ColorClient] success: %s reading from %s.\n",clientStr.c_str(),serverStr.c_str());
     return true;
 }
 
@@ -35,12 +35,12 @@ bool ColorClient::get(std::vector<double> &values) {
     if (!isQuiet) printf("[ColorClient] get()\n");
     Bottle *miInput = stateClient.read();  // read(false) not reading...
     if(!miInput) {
-        if (!isQuiet) fprintf(stderr,"[warning] Get failed, cannot read published state.\n");
+        if (!isQuiet) fprintf(stderr,"[ColorClient] warning: Get failed, cannot read published state.\n");
         return false;
     }
     if (!isQuiet) printf("[ColorClient] Got %d elems: %s\n",miInput->size(),miInput->toString().c_str());
     if(miInput->size() == 0) {
-        if (!isQuiet) fprintf(stderr,"[warning] Got empty contents, returning false.\n");
+        if (!isQuiet) fprintf(stderr,"[ColorClient] warning: Got empty contents, returning false.\n");
         return false;
     }
     for(int i=0;i<miInput->size();i++){
@@ -56,7 +56,12 @@ bool ColorClient::size(int &value) {
     if (!isQuiet) printf("[ColorClient] size()\n");
     Bottle *miInput = stateClient.read();  // read(false) not reading...
     if(!miInput) {
-        if (!isQuiet) fprintf(stderr,"[warning] Get failed, cannot read published state.\n");
+        if (!isQuiet) fprintf(stderr,"[ColorClient] warning: Get failed, cannot read published state.\n");
+        return false;
+    }
+    if(miInput->size() == 0) {
+        if (!isQuiet) fprintf(stderr,"[ColorClient] warning: Got empty contents, returning false and size -1.\n");
+        value = -1;
         return false;
     }
     Bottle *contents = miInput->get(0).asList();
