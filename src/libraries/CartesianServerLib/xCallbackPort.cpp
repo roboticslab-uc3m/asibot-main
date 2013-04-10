15e5dc38270f4704eded05dae46eb7616b71d22a
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-04-10 15:39:34 +0000

    gutsy move

--- src/libraries/CartesianServerLib/xCallbackPort.cpp
+++ src/libraries/CartesianServerLib/xCallbackPort.cpp
@@ -38,7 +38,10 @@ void xCallbackPort::onRead(Bottle& b) {
         cmd.push_back(lst->get(0).asDouble());
         cmd.push_back(lst->get(1).asDouble());
         Vector x,o,xd,od;
-        if(!icart->getPose(x,o)) return;
+        if(!icart->getPose(x,o)) {
+            fprintf(stderr,"[CartesianServerLib] warning: getPose failed!\n");
+            return;
+        }
         double ozDeg = (atan2(x[1],x[0]))*180.0/M_PI;
         double PrP = sqrt(x[0]*x[0]+x[1]*x[1]);
         double PrPd = PrP - VPOINT_DIST*sin((cmd[1])*M_PI/180.0); 
@@ -62,7 +65,10 @@ void xCallbackPort::onRead(Bottle& b) {
         cmd.push_back(lst->get(0).asDouble());
         cmd.push_back(lst->get(1).asDouble());
         Vector x,o,xd,od;
-        if(!icart->getPose(x,o)) return;
+        if(!icart->getPose(x,o)) {
+            fprintf(stderr,"[CartesianServerLib] warning: getPose failed!\n");
+            return;
+        }
         double ozDeg = (atan2(x[1],x[0]))*180.0/M_PI;
         double PrP = sqrt(x[0]*x[0]+x[1]*x[1]);
         double PrPd = PrP;
@@ -77,12 +83,35 @@ void xCallbackPort::onRead(Bottle& b) {
         *csStatus = 5;
         icart->goToPose(xd,od,3.0);
     } else if (choice==VOCAB_VMOS) { ///////////////////////////////// vmos /////////////////////////////////
-        Vector cmd;
         Bottle *lst = b.get(1).asList();
         Vector xdotd,odotd;
-        xdotd.push_back(lst->get(0).asDouble());
-        xdotd.push_back(lst->get(1).asDouble());
-        xdotd.push_back(lst->get(2).asDouble());
+        if (b.size()==2) {  // no tool param, just send in base ref
+            xdotd.push_back(lst->get(0).asDouble());
+            xdotd.push_back(lst->get(1).asDouble());
+            xdotd.push_back(lst->get(2).asDouble());
+        } else if (b.size()==3) {
+            Vector x,o;
+            if(!icart->getPose(x,o)) {
+                fprintf(stderr,"[CartesianServerLib] warning: getPose failed!\n");
+                return;
+            }
+            // Matrix axis2dcm(const Vector &v, unsigned int verbose);
+            Matrix H_base_ee = axis2dcm(o,0);
+            H_base_ee.resize(4,4);
+            H_base_ee(0,3) = x[0];
+            H_base_ee(1,3) = x[1];
+            H_base_ee(2,3) = x[2];
+            H_base_ee(3,3) = 1;
+            Vector xdotdP;
+            xdotdP.push_back(lst->get(0).asDouble());
+            xdotdP.push_back(lst->get(1).asDouble());
+            xdotdP.push_back(lst->get(2).asDouble());
+            xdotdP.push_back(1);
+            Vector xdotdPP = H_base_ee * xdotdP;
+            xdotd.push_back(xdotdPP[0]);
+            xdotd.push_back(xdotdPP[1]);
+            xdotd.push_back(xdotdPP[2]);
+        }
         for (int i = 3; i < lst->size(); i++)
             odotd.push_back(lst->get(i).asDouble());
         printf("xdotd: %s; odotd: %s\n",xdotd.toString().c_str(),odotd.toString().c_str());
@@ -119,6 +148,50 @@ void xCallbackPort::setPositionInterface(yarp::dev::IPositionControl* _ipos) {
 
 void xCallbackPort::setCsStatus(int* _csStatus) {
     csStatus = _csStatus;
+}
+
+/************************************************************************/
+
+Matrix xCallbackPort::axis2dcm(const Vector &v, unsigned int verbose) {
+
+    if (v.length()<4) {
+        if (verbose)
+            fprintf(stderr,"axis2dcm() failed\n");
+    
+        return Matrix(0,0);
+    }
+
+    Matrix R=eye(4,4);
+
+    double theta=v[3];
+    if (theta==0.0)
+        return R;
+
+    double c=cos(theta);
+    double s=sin(theta);
+    double C=1.0-c;
+
+    double xs =v[0]*s;
+    double ys =v[1]*s;
+    double zs =v[2]*s;
+    double xC =v[0]*C;
+    double yC =v[1]*C;
+    double zC =v[2]*C;
+    double xyC=v[0]*yC;
+    double yzC=v[1]*zC;
+    double zxC=v[2]*xC;
+    
+    R(0,0)=v[0]*xC+c;
+    R(0,1)=xyC-zs;
+    R(0,2)=zxC+ys;
+    R(1,0)=xyC+zs;
+    R(1,1)=v[1]*yC+c;
+    R(1,2)=yzC-xs;
+    R(2,0)=zxC-ys;
+    R(2,1)=yzC+xs;
+    R(2,2)=v[2]*zC+c;
+
+    return R;
 }
 
 /************************************************************************/
