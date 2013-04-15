4cff3f660a7e1a143c68ddeb4584a6ce95654501
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-04-15 13:10:47 +0000

    doxy it

--- src/libraries/ExtraMath/InlineMath.hpp
+++ src/libraries/ExtraMath/InlineMath.hpp
@@ -5,19 +5,23 @@
 
 //#include "math.h"
 
-// ----------------------------------------------------------------------------
-
+/**
+* Simple function to pass from radians to degrees.
+* @param inRad angle value in radians.
+* @return angle value in degrees.
+*/
 double toDeg(const double inRad) {
     return (inRad * 180.0 / M_PI);  // return (inRad * 180.0 / 3.14159265);
 }
 
-// ----------------------------------------------------------------------------
-
+/**
+* Simple function to pass from degrees to radians. 
+* @param inDeg angle value in degrees.
+* @return angle value in radians.
+*/
 double toRad(const double inDeg) {
     return (inDeg * M_PI / 180.0);  // return (inDeg * 3.14159265 / 180.0);
 }
-
-// ----------------------------------------------------------------------------
 
 #endif  // __INLINE_MATH__
 
