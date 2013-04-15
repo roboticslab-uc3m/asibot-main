f6b85a36fd65fee259c00e7893b8ff89c22b6a38
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2013-04-15 13:36:10 +0000

    =

--- asibot/main/src/libraries/ExtraMath/TinyMath.hpp
+++ asibot/main/src/libraries/ExtraMath/TinyMath.hpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#ifndef __INLINE_MATH__
-#define __INLINE_MATH__
+#ifndef __TINY_MATH__
+#define __TINY_MATH__
 
 //#include "math.h"
 
@@ -10,18 +10,14 @@
 * @param inRad angle value in radians.
 * @return angle value in degrees.
 */
-double toDeg(const double inRad) {
-    return (inRad * 180.0 / M_PI);  // return (inRad * 180.0 / 3.14159265);
-}
+double toDeg(const double inRad);
 
 /**
 * Simple function to pass from degrees to radians. 
 * @param inDeg angle value in degrees.
 * @return angle value in radians.
 */
-double toRad(const double inDeg) {
-    return (inDeg * M_PI / 180.0);  // return (inDeg * 3.14159265 / 180.0);
-}
+double toRad(const double inDeg);
 
-#endif  // __INLINE_MATH__
+#endif  // __TINY_MATH__
 
