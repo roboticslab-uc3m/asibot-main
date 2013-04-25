d5ab8b4d7a2f0662ba06018d50b4601ecda43350
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-04-25 13:43:37 +0000

    just some debuging info

--- src/libraries/RlPlugins/ravebot/RateThreadImpl.cpp
+++ src/libraries/RlPlugins/ravebot/RateThreadImpl.cpp
@@ -21,19 +21,19 @@ void RaveBot::run() {
         if((jointStatus[motor]==1)||(jointStatus[motor]==2)||(jointStatus[motor]==3)) {  // if set to move...
             if ((getEncExposed(motor) > maxLimit[motor])  && (velRaw[motor]>0)) {  // SW max JL
                 stop(motor);  // puts jointStatus[motor]=0;
-                printf("[warning] Moving joint q%d at configured max joint limit, stopping.\n",motor+1);
+                fprintf(stderr,"[RaveBot] warning: Moving joint q%d at configured max joint limit, stopping.\n",motor+1);
             } else if ((getEncExposed(motor) < minLimit[motor]) && (velRaw[motor]<0)) {  // SW min JL
                 stop(motor);  // puts jointStatus[motor]=0;
-                printf("[warning] Moving joint q%d at configured min joint limit, stopping.\n",motor+1);
+                fprintf(stderr,"[RaveBot] warning: Moving joint q%d at configured min joint limit, stopping.\n",motor+1);
             } else if((jointStatus[motor]==1)||(jointStatus[motor]==2)) {  // check if target reached in pos or rel
                 if ( (velRaw[motor] > 0) &&  // moving positive...
                     (getEncExposed(motor) > (targetExposed[motor]-jointTol[motor])) ) {
                     stop(motor);  // puts jointStatus[motor]=0;
-                    printf("Joint q%d reached target.\n",motor+1);
+                    printf("[RaveBot] Joint q%d reached target.\n",motor+1);
                 } else if ( (velRaw[motor] < 0) &&  // moving negative...
                     (getEncExposed(motor) < (targetExposed[motor]+jointTol[motor])) ) {
                     stop(motor);  // puts jointStatus[motor]=0;
-                    printf("Joint q%d reached target.\n",motor+1);
+                    printf("[RaveBot] Joint q%d reached target.\n",motor+1);
                 }
             }
         }
