564d0e400919b22117696747fd4be9d8fc99da2b
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2013-06-15 18:09:19 +0000

    gone

--- asibot/main/src/modules/visionDepth/SegmentorThread.cpp
+++ asibot/main/src/modules/visionDepth/SegmentorThread.cpp
@@ -124,7 +124,7 @@ void SegmentorThread::run() {
     Travis travis;  // quiet, overwrite (faster)
     travis.setCvMat(inCvMat);
     travis.binarize(algorithm, threshold);
-    travis.morphClosing( inYarpImg->width() * 0.1 ); // 4 for 100, very rule-of-thumb
+    travis.morphClosing( inYarpImg->width() * 0.05 ); // 4 for 100, very rule-of-thumb
     travis.blobize(maxNumBlobs);
     vector<cv::Point> blobsXY;
     travis.getBlobsXY(blobsXY);
