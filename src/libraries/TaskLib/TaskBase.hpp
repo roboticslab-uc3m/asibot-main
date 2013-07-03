f5a90b6ddadf81223375a845b63163edf8883579
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-07-03 08:34:12 +0000

    incorporate in project

---
+++ src/libraries/TaskLib/TaskBase.hpp
@@ -0,0 +1,31 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __TASK_BASE_HPP__
+#define __TASK_BASE_HPP__
+
+#include <stdio.h>
+
+/**
+ * @ingroup asibot_libraries
+ *
+ * \defgroup TaskLib
+ *
+ * @brief The TaskLib library is a collection of a simple lib for hard-coded (maybe not so much) task classes.
+ *
+ * The TaskLib library is a collection of a simple lib for hard-coded (maybe not so much) task classes.
+ *
+ */
+
+/**
+ * @ingroup TaskLib
+ *
+ * @brief A base class for these tasks.
+ *
+ */
+class TaskBase {
+  public:
+    virtual bool configure(const double xi, const double xf, const double _T) = 0;
+};
+
+#endif  // __TASK_BASE_HPP__
+
