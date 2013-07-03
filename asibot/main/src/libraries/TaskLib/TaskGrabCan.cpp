a321cdbaf3a1b5a9e7d303f4d21d8e688860a862
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2013-07-03 09:00:49 +0000

    more stub

---
+++ asibot/main/src/libraries/TaskLib/TaskGrabCan.cpp
@@ -0,0 +1,18 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "TaskGrabCan.hpp"
+
+/************************************************************************/
+bool TaskGrabCan::run() {
+    printf("[TaskGrabCan] succeess: begin{run()}\n");
+    {
+        double targets[5] = {0,-0.3,0.9,90,0};
+        printf("Movj to targets: {0,-0.3, 0.9, 90.0, 0.0}...\n");
+        cartesianClient.movj(targets);
+    }
+    printf("[TaskGrabCan] succeess: end{run()}\n");
+    return true;
+}
+
+/************************************************************************/
+
