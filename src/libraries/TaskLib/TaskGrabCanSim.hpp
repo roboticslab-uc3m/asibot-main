3554d7a1b5673cf9cce20381c8afbdaee92a9389
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-07-13 17:11:06 +0000

    more classes

---
+++ src/libraries/TaskLib/TaskGrabCanSim.hpp
@@ -0,0 +1,30 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __TASK_GRAB_CAN_HPP__
+#define __TASK_GRAB_CAN_HPP__
+
+#include "TaskBase.hpp"
+
+#include "CartesianClient.h"
+#include <yarp/os/RpcClient.h>
+
+/**
+ * @ingroup TaskGrabCan
+ *
+ * The TaskGrabCan class implements a client part that connects to a remote
+ * \ref cartesianServer module. Warning: Be careful with NUM_AXES, depends on angle representation!
+ * 
+ */
+class TaskGrabCan : TaskBase {
+  protected:
+    CartesianClient cartesianClient;
+    RpcClient worldRpcClient;
+
+  public:
+    virtual bool configure(yarp::os::Searchable& config);
+    virtual bool run();
+    virtual bool close();
+};
+
+#endif  // __TASK_GRAB_CAN_HPP__
+
