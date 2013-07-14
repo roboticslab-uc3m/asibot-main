7757af665dc44b8613a12c1cd98c2854ddbe9eaf
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-07-14 11:26:45 +0000

    clear out

--- src/libraries/TaskLib/TaskGrabCanSim.cpp
+++ src/libraries/TaskLib/TaskGrabCanSim.cpp
@@ -8,7 +8,9 @@ bool TaskGrabCanSim::configure(yarp::os::Searchable& config) {
 
     _quiet = false;
     if (config.check("quiet")) _quiet = true;
+    if (config.check("robotAngle")) _robotAngle = config.find("robotAngle").asDouble();
 
+    if (!_quiet) 
     if (!_quiet) printf("[TaskGrabCanSim] success: end{configure()}\n");    
     return true;
 }
@@ -16,27 +18,27 @@ bool TaskGrabCanSim::configure(yarp::os::Searchable& config) {
 /************************************************************************/
 bool TaskGrabCanSim::run() {
     if (!_quiet) printf("[TaskGrabCanSim] success: begin{run()}\n");
-    if (!cartesianClient.open("/ravebot")) return false;
-    if (!worldRpcClient.open("/taskGrabCanSim/world:o")) return false;
-    if (!worldRpcClient.addOutput("/ravebot/world")) return false;
+    if (!_cartesianClient.open("/ravebot")) return false;
+    if (!_worldRpcClient.open("/taskGrabCanSim/world:o")) return false;
+    if (!_worldRpcClient.addOutput("/ravebot/world")) return false;
     Bottle bOut, bIn;
     bOut.addString("world");
     bOut.addString("whereis");
     bOut.addString("obj");
     bOut.addString("redCan");
-    worldRpcClient.write(bOut, bIn);
+    _worldRpcClient.write(bOut, bIn);
     //printf("[TaskGrabCanSim] Got info on redCan: %s\n",bIn.toString().c_str());
     Bottle* data = bIn.get(0).asList();
     double x = data->get(0).asDouble();
     double y = data->get(1).asDouble();
     double z = data->get(2).asDouble();
-    printf("[TaskGrabCanSim] redCan at %f %f %f.\n",x,y,z);
+    printf("[TaskGrabCanSim] world says redCan at %f %f %f.\n",x,y,z);
 
 
     {
         double targets[5] = {0,-0.3,0.9,90,0};
-        printf("Movj to targets: {0,-0.3, 0.9, 90.0, 0.0}...\n");
-        cartesianClient.movj(targets);
+        printf("[TaskGrabCanSim] Commanding Movj to targets: {0,-0.3, 0.9, 90.0, 0.0}...\n");
+        _cartesianClient.movj(targets);
     }
     if (!_quiet) printf("[TaskGrabCanSim] success: end{run()}\n");
     return true;
@@ -45,6 +47,8 @@ bool TaskGrabCanSim::run() {
 /************************************************************************/
 bool TaskGrabCanSim::close() {
     if (!_quiet) printf("[TaskGrabCanSim] success: begin{close()}\n");
+    _worldRpcClient.interrupt();
+    _cartesianClient.close();
     if (!_quiet) printf("[TaskGrabCanSim] success: end{close()}\n");
     return true;
 }
