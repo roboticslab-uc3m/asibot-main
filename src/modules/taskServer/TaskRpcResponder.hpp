2e38d904ec543a467164ff0befe4ae6788eca01a
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-07-17 11:28:32 +0000

    cleaning

--- src/modules/taskServer/TaskRpcResponder.hpp
+++ src/modules/taskServer/TaskRpcResponder.hpp
@@ -1,31 +1,19 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#ifndef __X_RPC_RESPONDER__
-#define __X_RPC_RESPONDER__
+#ifndef __TASK_RPC_RESPONDER__
+#define __TASK_RPC_RESPONDER__
 
 #include <yarp/os/Port.h>
 #include <yarp/os/BufferedPort.h>
 #include <yarp/os/Time.h>
+#include <yarp/os/Vocab.h>
 
-#include <yarp/dev/PolyDriver.h>
-#include <yarp/dev/CartesianControl.h>
-#include <yarp/dev/ControlBoardInterfaces.h>
-
-#define MAX_NUM_MOTORS 100
+//#define MAX_NUM_MOTORS 100
 
 #define VOCAB_HELP VOCAB4('h','e','l','p')
-#define VOCAB_MY_STOP VOCAB4('s','t','o','p')
-#define VOCAB_STAT VOCAB4('s','t','a','t')
-#define VOCAB_MOVL VOCAB4('m','o','v','l')
-#define VOCAB_MOVJ VOCAB4('m','o','v','j')
-#define VOCAB_INV VOCAB3('i','n','v')
-#define VOCAB_MOVE VOCAB4('m','o','v','e')
-#define VOCAB_WAIT VOCAB4('w','a','i','t')
-#define VOCAB_TOOL VOCAB4('t','o','o','l')
+#define VOCAB_FAILED VOCAB4('f','a','i','l')
 
 using namespace yarp::os;
-using namespace yarp::dev;
-using yarp::sig::Vector;
 
 /**
  * @ingroup TaskRpcResponder
@@ -39,27 +27,15 @@ protected:
     */
     virtual bool read(ConnectionReader& connection);
 
-    yarp::dev::ICartesianControl *icart;
-    yarp::dev::IPositionControl *ipos;
     int *tsStatus;
 
 public:
 
     /**
-    * Register a cartesian interface for the PortReader.
-    */
-    void setCartesianInterface(yarp::dev::ICartesianControl* _icart);
-
-    /**
-    * Register a position interface for the PortReader.
-    */
-    void setPositionInterface(yarp::dev::IPositionControl* _ipos);
-
-    /**
     * Register.
     */
-    void setCsStatus(int* _tsStatus);
+    void setTsStatus(int* _tsStatus);
 };
 
-#endif
+#endif  // __TASK_RPC_RESPONDER__
 
