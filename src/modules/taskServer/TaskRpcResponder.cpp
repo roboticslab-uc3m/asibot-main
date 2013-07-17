6abcc31ddea0da804a8e69dc3db208933c269859
Juan G. Victores <jgvictores@users.noreply.github.com>
2013-07-17 11:47:56 +0000

    run working

--- src/modules/taskServer/TaskRpcResponder.cpp
+++ src/modules/taskServer/TaskRpcResponder.cpp
@@ -12,19 +12,22 @@ bool TaskRpcResponder::read(ConnectionReader& connection) {
     ConnectionWriter *returnToSender = connection.getWriter();
     if (returnToSender==NULL) return false;
     if ((in.get(0).asString() == "help")||(in.get(0).asVocab() == VOCAB_HELP)) {  // help //
-        out.addString("Available commands: [help] [inv] [movj] [movl] [stat] [stop] [wait] [tool] more info at [http://roboticslab.sourceforge.net/asibot/group__cartesianServer.html]");
+        out.addString("Available commands: [help] [run] more info at [http://roboticslab.sourceforge.net/asibot/group__cartesianServer.html]");
         return out.write(*returnToSender);
-    } /*else if ((in.get(0).asString() == "stop")||(in.get(0).asVocab() == VOCAB_STOP)) {  // stop //
-        if(icart->stopControl()) {
-            if(ipos->stop()) {
-                if(ipos->setPositionMode()) {
-                    out.addVocab(VOCAB_OK);
-                    *tsStatus = 0;
-                } else out.addVocab(VOCAB_FAILED);
-            } else out.addVocab(VOCAB_FAILED);
-        } else out.addVocab(VOCAB_FAILED);
+    } else if ((in.get(0).asString() == "run")||(in.get(0).asVocab() == VOCAB_RUN)) {  // run //
+        Property options;
+        options.put("robotAngle",30.0);
+        taskGrabCanSim.configure(options);
+        if(taskGrabCanSim.run() ) {
+            printf("[TaskRpcResponder] success: taskGrabCanSim.run()\n");
+            out.addVocab(VOCAB_OK);
+        } else {
+            fprintf(stderr,"[TaskRpcResponder] failed: taskGrabCanSim.run()\n");
+            out.addVocab(VOCAB_FAILED);
+        }
+        taskGrabCanSim.close();
         return out.write(*returnToSender);
-    } */ else {
+    } else {
         fprintf(stderr,"[TaskRpcResponder] fail: Unknown command (use 'help' if needed).\n");
         out.addVocab(VOCAB_FAILED);
         return out.write(*returnToSender);
