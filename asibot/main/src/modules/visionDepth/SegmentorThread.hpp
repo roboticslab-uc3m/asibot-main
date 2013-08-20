08d0fb61a004674c33eeaeff63de25d3868c261a
jgvictores <jgvictores@b561ad57-c185-4d47-9c4b-495d268d2a83>
2013-08-20 17:05:09 +0000

    fix defaults

--- asibot/main/src/modules/visionDepth/SegmentorThread.hpp
+++ asibot/main/src/modules/visionDepth/SegmentorThread.hpp
@@ -43,7 +43,7 @@
 #define DEFAULT_MAX_NUM_BLOBS 2
 #define DEFAULT_MORPH_CLOSING 2
 //#define DEFAULT_OUT_FEATURES "rawX rawY locX locY locZ"  // it's a bottle!!
-#define DEFAULT_OUT_FEATURES "locX_0 locY_0 locY_0 angle area"  // it's a bottle!!
+#define DEFAULT_OUT_FEATURES "locX_0 locY_0 locZ_0 angle area"  // it's a bottle!!
 #define DEFAULT_OUT_FEATURES_FORMAT 0  // 0=bottled,1=minimal
 #define DEFAULT_OUT_IMAGE 1
 #define DEFAULT_RATE_MS 20
