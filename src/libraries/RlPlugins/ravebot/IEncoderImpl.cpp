fc26336c24434f6c0ecbdd283ec1ebb9b51793f8
teo <jgvictores@github.com>
2015-03-11 12:40:10 +0100

    RaveBot IEncoders updated to IEncodersTimed so cb can be updated to cbw2

--- src/libraries/RlPlugins/ravebot/IEncoderImpl.cpp
+++ src/libraries/RlPlugins/ravebot/IEncoderImpl.cpp
@@ -81,3 +81,24 @@ bool RaveBot::getEncoderAccelerations(double *accs) {
 
 // -----------------------------------------------------------------------------
 
+bool RaveBot::getEncodersTimed(double *encs, double *time) {
+    //CD_INFO("\n");  //-- Way too verbose
+
+    bool ok = true;
+    for(unsigned int i=0; i < numMotors; i++)
+        ok &= getEncoderTimed(i,&(encs[i]),&(time[i]));
+    return ok;
+}
+
+// -----------------------------------------------------------------------------
+
+bool RaveBot::getEncoderTimed(int j, double *encs, double *time) {
+    //CD_INFO("(%d)\n",j);  //-- Way too verbose
+
+    getEncoder(j, encs);
+    *time = yarp::os::Time::now();
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
