a0bc2315e3d5f4acc98606a3fd04fb9568651d9b
Bartek Łukawski <bwmn.peter@gmail.com>
2016-10-21 20:26:53 +0200

    Rename RaveBot's position/velocity mode vocabs, update .xml.template(s)
    For consistency with latest commits since previous names
    (VOCAB_POSITION_MODE, VOCAB_VELOCITY_MODE) are mainly associated in YARP
    code with now-deprecated interface methods.

--- src/libraries/RlPlugins/ravebot/IPositionImpl.cpp
+++ src/libraries/RlPlugins/ravebot/IPositionImpl.cpp
@@ -21,7 +21,7 @@ bool RaveBot::getAxes(int *ax) {
 
 bool RaveBot::positionMove(int j, double ref) {  // encExposed = ref;
     if ((unsigned int)j>numMotors) return false;
-    if(vModePosVel[j]!=VOCAB_POSITION_MODE) {  // Check if we are in position mode.
+    if(vModePosVel[j]!=VOCAB_CM_POSITION) {  // Check if we are in position mode.
         fprintf(stderr,"[RaveBot] warning: will not positionMove as joint %d not in positionMode\n",j+1);
         return false;
     }
@@ -48,7 +48,7 @@ bool RaveBot::positionMove(int j, double ref) {  // encExposed = ref;
 
 bool RaveBot::positionMove(const double *refs) {  // encExposed = refs;
     for(unsigned int motor=0;motor<numMotors;motor++) {
-        if(vModePosVel[motor]!=VOCAB_POSITION_MODE) {  // Check if we are in position mode.
+        if(vModePosVel[motor]!=VOCAB_CM_POSITION) {  // Check if we are in position mode.
             fprintf(stderr,"[RaveBot] error: Will not positionMove as joint %d not in positionMode\n",motor+1);
             return false;
         }
@@ -85,7 +85,7 @@ bool RaveBot::positionMove(const double *refs) {  // encExposed = refs;
 
 bool RaveBot::relativeMove(int j, double delta) {
     if ((unsigned int)j>numMotors) return false;
-    if(vModePosVel[j]!=VOCAB_POSITION_MODE) {  // Check if we are in position mode.
+    if(vModePosVel[j]!=VOCAB_CM_POSITION) {  // Check if we are in position mode.
         printf("[fail] RaveBot will not relativeMove as joint %d not in positionMode\n",j+1);
         return false;
     }
@@ -112,7 +112,7 @@ bool RaveBot::relativeMove(int j, double delta) {
 
 bool RaveBot::relativeMove(const double *deltas) {  // encExposed = deltas + encExposed
     for(unsigned int motor=0;motor<numMotors;motor++) {
-        if(vModePosVel[motor]!=VOCAB_POSITION_MODE) {  // Check if we are in position mode.
+        if(vModePosVel[motor]!=VOCAB_CM_POSITION) {  // Check if we are in position mode.
             fprintf(stderr,"[RaveBot] warning: will not relativeMove as joint %d not in positionMode\n",motor+1);
             return false;
         }
