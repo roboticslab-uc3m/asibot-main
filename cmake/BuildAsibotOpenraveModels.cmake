23b17f7ebcf9e97ddf9f70762386dd219b5ea730
Bartek Łukawski <bwmn.peter@gmail.com>
2017-02-22 16:25:56 +0100

    Create BuildAsibotOpenraveModels.cmake

---
+++ cmake/BuildAsibotOpenraveModels.cmake
@@ -0,0 +1,6 @@
+include(YCMEPHelper)
+
+ycm_ep_helper(AsibotOpenraveModels TYPE GIT
+              STYLE GITHUB
+              REPOSITORY roboticslab-uc3m/asibot-openrave-models.git
+              TAG master)
