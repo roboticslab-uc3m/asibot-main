ef339555868245f23d562fd47320570d0261b2b4
Bartek Łukawski <bwmn.peter@gmail.com>
2017-02-25 01:32:53 +0100

    Fix user package registry detection, rename pkg
    YCMEPHelper module should not be included in main
    CMakeLists.txt since it spoils the user registry
    detection (populated by export(PACKAGE)).

--- cmake/BuildAsibotOpenRAVEModels.cmake
+++ cmake/BuildAsibotOpenRAVEModels.cmake
@@ -1,6 +1,6 @@
 include(YCMEPHelper)
 
-ycm_ep_helper(AsibotOpenraveModels TYPE GIT
+ycm_ep_helper(AsibotOpenRAVEModels TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/asibot-openrave-models.git
               TAG master)
