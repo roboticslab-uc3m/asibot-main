09da12d388e101d67e66eb5cfb14ff7446eb9ff0
Bartek Łukawski <bwmn.peter@gmail.com>
2017-03-17 12:58:18 +0100

    Apply naming conventions to OpenRAVE models repo
    https://github.com/roboticslab-uc3m/best-practices/issues/5

--- cmake/BuildASIBOT_OPENRAVE_MODELS.cmake
+++ cmake/BuildASIBOT_OPENRAVE_MODELS.cmake
@@ -1,6 +1,6 @@
 include(YCMEPHelper)
 
-ycm_ep_helper(AsibotOpenRAVEModels TYPE GIT
+ycm_ep_helper(ASIBOT_OPENRAVE_MODELS TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/asibot-openrave-models.git
               TAG master)
