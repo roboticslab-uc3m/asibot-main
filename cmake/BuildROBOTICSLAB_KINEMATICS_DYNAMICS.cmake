9c5d73d7a54727aefa4c0ba377318a778446f8f2
Bartek Łukawski <bwmn.peter@gmail.com>
2017-03-31 14:26:07 +0200

    Add YCM Build*.cmake files for kinematics-dynamics and openrave-* repos

---
+++ cmake/BuildROBOTICSLAB_KINEMATICS_DYNAMICS.cmake
@@ -0,0 +1,6 @@
+include(YCMEPHelper)
+
+ycm_ep_helper(ROBOTICSLAB_KINEMATICS_DYNAMICS TYPE GIT
+              STYLE GITHUB
+              REPOSITORY roboticslab-uc3m/kinematics-dynamics.git
+              TAG master)
---
+++ cmake/BuildROBOTICSLAB_OPENRAVE_YARP_PLUGINS.cmake
@@ -0,0 +1,6 @@
+include(YCMEPHelper)
+
+ycm_ep_helper(ROBOTICSLAB_OPENRAVE_YARP_PLUGINS TYPE GIT
+              STYLE GITHUB
+              REPOSITORY roboticslab-uc3m/openrave-yarp-plugins.git
+              TAG master)
