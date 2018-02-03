64e8015479f9567db10d877872947547afe31acb
Bartek Łukawski <bwmn.peter@gmail.com>
2018-02-03 19:16:12 +0100

    Finalize superbuild tweaks

---
+++ cmake/ycm-modules/BuildROBOTICSLAB_INSTALLATION_GUIDES.cmake
@@ -0,0 +1,6 @@
+include(YCMEPHelper)
+
+ycm_ep_helper(ROBOTICSLAB_INSTALLATION_GUIDES TYPE GIT
+              STYLE GITHUB
+              REPOSITORY roboticslab-uc3m/installation-guides.git
+              TAG master)
