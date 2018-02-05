ed9b4e3002e149100903a745e00a9c52b8a1eb11
Bartek Łukawski <bwmn.peter@gmail.com>
2018-02-05 11:59:03 +0100

    Pull deep dependencies via YCM, keep Travis simple

--- cmake/ycm-modules/BuildASIBOT_HMI.cmake
+++ cmake/ycm-modules/BuildASIBOT_HMI.cmake
@@ -1,10 +1,11 @@
 include(YCMEPHelper)
 include(FindOrBuildPackage)
 
+find_or_build_package(YARP QUIET)
 find_or_build_package(ROBOTICSLAB_KINEMATICS_DYNAMICS QUIET)
 
 ycm_ep_helper(ASIBOT_HMI TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/asibot-hmi.git
               TAG master
-              DEPENDS ROBOTICSLAB_KINEMATICS_DYNAMICS)
+              DEPENDS "YARP;ROBOTICSLAB_KINEMATICS_DYNAMICS")
--- cmake/ycm-modules/BuildROBOTICSLAB_YARP_DEVICES.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_YARP_DEVICES.cmake
@@ -1,6 +1,10 @@
 include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
 
 ycm_ep_helper(ROBOTICSLAB_YARP_DEVICES TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/yarp-devices.git
-              TAG master)
+              TAG master
+              DEPENDS YARP)
