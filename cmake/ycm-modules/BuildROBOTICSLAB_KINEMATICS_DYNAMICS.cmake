a7007a0fd9f3eaac2fa8683cadb4f8d77a7a9da9
Bartek Łukawski <bwmn.peter@gmail.com>
2018-06-18 18:33:54 +0200

    Pull color-debug via YCM

--- cmake/ycm-modules/BuildROBOTICSLAB_KINEMATICS_DYNAMICS.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_KINEMATICS_DYNAMICS.cmake
@@ -3,9 +3,10 @@ include(FindOrBuildPackage)
 
 find_or_build_package(orocos_kdl QUIET)
 find_or_build_package(YARP QUIET)
+find_or_build_package(COLOR_DEBUG QUIET)
 
 ycm_ep_helper(ROBOTICSLAB_KINEMATICS_DYNAMICS TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/kinematics-dynamics.git
               TAG master
-              DEPENDS "orocos_kdl;YARP")
+              DEPENDS "orocos_kdl;YARP;COLOR_DEBUG")
