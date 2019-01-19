0259f65fb5c7043319ca0f8d0cc212f15d171b09
Bartek Łukawski <PeterBowman@users.noreply.github.com>
2019-01-19 16:56:39 +0100

    Disable OR videorecording
    https://github.com/roboticslab-uc3m/installation-guides/issues/56

--- cmake/ycm-modules/BuildOpenRAVE.cmake
+++ cmake/ycm-modules/BuildOpenRAVE.cmake
@@ -4,7 +4,7 @@ ycm_ep_helper(OpenRAVE TYPE GIT
               STYLE GITHUB
               REPOSITORY rdiankov/openrave.git
               TAG master
-              CMAKE_CACHE_ARGS "-DOPENRAVE_PLUGIN_FCLRAVE:BOOL=OFF"
+              CMAKE_CACHE_ARGS "-DOPENRAVE_PLUGIN_FCLRAVE:BOOL=OFF;-DOPT_VIDEORECORDING:BOOL=OFF"
               # in CMake 3.6+, use: GIT_SHALLOW TRUE
               # note: does not perform --recursive (CMake 3.5, YCM 0.9)
               DOWNLOAD_COMMAND git clone --depth 1 https://github.com/rdiankov/openrave.git OpenRAVE)
