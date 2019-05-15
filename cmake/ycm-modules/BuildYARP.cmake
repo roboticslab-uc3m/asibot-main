657c3562358956da2ebafd127d6c606650d14dfe
Bartek Łukawski <PeterBowman@users.noreply.github.com>
2019-05-15 13:13:46 +0200

    Remove default-enabled YARP_math lib option

--- cmake/ycm-modules/BuildYARP.cmake
+++ cmake/ycm-modules/BuildYARP.cmake
@@ -4,5 +4,5 @@ ycm_ep_helper(YARP TYPE GIT
               STYLE GITHUB
               REPOSITORY robotology/yarp.git
               TAG master
-              CMAKE_CACHE_ARGS "-DSKIP_ACE:BOOL=ON;-DCREATE_LIB_MATH:BOOL=ON"
+              CMAKE_CACHE_ARGS "-DSKIP_ACE:BOOL=ON"
               GIT_PROGRESS TRUE)
